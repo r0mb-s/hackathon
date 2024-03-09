d0f8e45931cc60b28ffded0ecb0223694e06a728
Togoe-Radu-Mihai <radu.togoe03@e-uvt.ro>
2024-03-09 20:03:57 +0200

    Commited the Android App at 8pm

---
+++ android_app/smartcity/app/src/main/java/com/example/smartcity/Pinpoint.java
@@ -0,0 +1,14 @@
+package com.example.smartcity;
+
+public class Pinpoint {
+    public double latitude;
+    public double longitude;
+    public double percentage;
+
+    public Pinpoint(double latitude, double longitude, double percentage) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.percentage = percentage;
+    }
+
+}
